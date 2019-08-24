d1d9545c7bd7c55e729cd6781fc25420cab86a60
Elizabeth Smalls <lizsmalls@hotmail.com>
2019-08-24 16:31:12 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,3 @@
 # Define the variable board below.
-board = ["str1", "str2", "str3", "str4,", "str5","str6", "str7", "str8", "str9"]
+board = ["", "str2", "str3", "str4,", "str5","str6", "str7", "str8", "str9"]
 
